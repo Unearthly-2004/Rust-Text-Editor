e4b9d4b1b016f8e793c4aca2b434b39c07b1da19
Shrujan Kharwadey <kharwadeyshrujan@gmail.com>
2025-06-17 00:20:20 +0530

    added changes to editor file

--- rust-text-editor/hecto/target/release/deps/hecto.d
+++
@@ -1,5 +0,0 @@
-E:\projects\rust\rust-text-editor\hecto\target\release\deps\hecto.exe: src\main.rs
-
-E:\projects\rust\rust-text-editor\hecto\target\release\deps\hecto.d: src\main.rs
-
-src\main.rs:
--- rust-text-editor/hecto/target/release/hecto.d
+++
@@ -1,1 +0,0 @@
-E:\projects\rust\rust-text-editor\hecto\target\release\hecto.exe: E:\projects\rust\rust-text-editor\hecto\src\main.rs
