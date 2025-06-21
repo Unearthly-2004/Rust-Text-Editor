0df0b13bd844ade68c475324d41faf14fd02dcf5
Shrujan Kharwadey <kharwadeyshrujan@gmail.com>
2025-06-21 23:36:27 +0530

    now editor is updated with a tilde boundary

--- rust-text-editor/hecto/target/debug/deps/hecto.d
+++ rust-text-editor/hecto/target/debug/deps/hecto.d
@@ -1,6 +1,7 @@
-E:\projects\rust\rust-text-editor\hecto\target\debug\deps\hecto.exe: src\main.rs src\editor.rs
+E:\projects\rust\rust-text-editor\hecto\target\debug\deps\hecto.exe: src\main.rs src\editor.rs src\editor\terminal.rs
 
-E:\projects\rust\rust-text-editor\hecto\target\debug\deps\hecto.d: src\main.rs src\editor.rs
+E:\projects\rust\rust-text-editor\hecto\target\debug\deps\hecto.d: src\main.rs src\editor.rs src\editor\terminal.rs
 
 src\main.rs:
 src\editor.rs:
+src\editor\terminal.rs:
