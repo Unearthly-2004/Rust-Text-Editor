2ffff134c6a113e73909c2bbee96ffdde4b68908
Shrujan Kharwadey <kharwadeyshrujan@gmail.com>
2025-06-29 00:35:11 +0530

    made layout changes

--- rust-text-editor/hecto/src/editor/pretty_editor.rs
+++ rust-text-editor/hecto/src/editor/pretty_editor.rs
@@ -1,8 +1,7 @@
-use std::io::{self, stdout, Read, Write};
+use std::io::{self, Read, Write};
 use std::time::{SystemTime, UNIX_EPOCH};
 
 pub struct PrettyEditor {
-    quit: bool,
     rows: Vec<String>,
     cursor: usize,
 }
@@ -10,56 +9,43 @@ pub struct PrettyEditor {
 impl PrettyEditor {
     pub fn new() -> Self {
         Self {
-            quit: false,
             rows: vec![
                 "Welcome to Pretty Editor!".into(),
                 "Use 'j'/'k' to move, 'q' to quit.".into(),
-                "Current line is highlighted.".into(),
-                "Feel free to modify this!".into(),
+                "This is a compact version.".into(),
             ],
             cursor: 0,
         }
     }
 
     pub fn run(&mut self) {
-        while !self.quit {
-            self.refresh().unwrap();
-            self.keypress();
-        }
+        while self.draw().is_ok() && self.input() {}
     }
 
-    fn keypress(&mut self) {
-        let mut buf = [0; 1];
-        if io::stdin().read_exact(&mut buf).is_ok() {
-            match buf[0] {
-                b'q' => self.quit = true,
-                b'j' if self.cursor + 1 < self.rows.len() => self.cursor += 1,
-                b'k' if self.cursor > 0 => self.cursor -= 1,
-                _ => {}
-            }
+    fn input(&mut self) -> bool {
+        let mut b = [0]; io::stdin().read_exact(&mut b).ok()?;
+        match b[0] {
+            b'q' => false,
+            b'j' if self.cursor + 1 < self.rows.len() => self.cursor += 1,
+            b'k' if self.cursor > 0 => self.cursor -= 1,
+            _ => {}
         }
+        true
     }
 
-    fn refresh(&self) -> io::Result<()> {
-        let mut out = stdout();
+    fn draw(&self) -> io::Result<()> {
         print!("\x1b[2J\x1b[H"); // clear screen
-        for (i, row) in self.rows.iter().enumerate() {
-            let line = format!("{:>3} ", i + 1);
-            if i == self.cursor {
-                print!("\x1b[7m\x1b[2m{}{}\x1b[0m\r\n", line, row);
+        for (i, r) in self.rows.iter().enumerate() {
+            let num = format!("{:>3} ", i + 1);
+            let row = if i == self.cursor {
+                format!("\x1b[7m\x1b[2m{}{}\x1b[0m", num, r)
             } else {
-                print!("\x1b[2m{}\x1b[0m{}\r\n", line, row);
-            }
+                format!("\x1b[2m{}{}\x1b[0m", num, r)
+            };
+            println!("{}", row);
         }
-
         let time = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
-        print!(
-            "\x1b[7m Line {} | {} lines | Time: {} \x1b[0m\r\n",
-            self.cursor + 1,
-            self.rows.len(),
-            time
-        );
-
-        out.flush()
+        println!("\x1b[7m Line {} | {} lines | Time: {} \x1b[0m", self.cursor + 1, self.rows.len(), time);
+        io::stdout().flush()
     }
 }
